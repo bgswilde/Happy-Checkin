2ac4b214c3960d4e8528c534bb949da53bfc1db1
Brian Kaufman <bpkaufman4@gmail.com>
2021-11-18 13:01:01 -0600

    created feedback schema

---
+++ server/models/Feedback.js
@@ -0,0 +1,15 @@
+const { Schema } = require('mongoose');
+
+const FeedbackSchema = new Schema({
+    rating: {
+        type: Number,
+        max: 5
+    },
+    body: {
+        type: String,
+        minlength: 1,
+        maxlength: 300
+    }
+})
+
+module.exports = FeedbackSchema;
