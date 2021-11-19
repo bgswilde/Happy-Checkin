54aa2b81e766412cf017a1e7be9de703539390eb
Terror666 <ckuthulu@gmail.com>
2021-11-19 17:06:03 -0600

    merge conflict

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@
 import LoginForm from './components/LoginForm';
 import SignupForm from './components/SignupForm';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-<<<<<<< HEAD
 import Header from './components/Header';
 import Footer from './components/Footer';
+import About from './pages/About';
 
 function App() {
   return (
@@ -20,32 +20,13 @@ function App() {
         <Router>
           <Switch>
             <Route exact path="/" component={LoginForm} />
-            <Route exact path="/signup" element={SignupForm} />
+            <Route exact path="/signup" component={SignupForm} />
+            <Route exact path="/about" component={About} />
           </Switch>
         </Router>
       </div>
       <Footer />
-=======
-import About from './pages/About';
-
-function App() {
-  return (
-    <div className="App">
-      {/* {(loggedIn && user.role === 0) ? 
-        <CustomerDashboard /> :
-      (loggedIn && user.role === 1) ? 
-        <CheckerDashboard /> :
-        <LoginForm />
-      } */}
-      <Router>
-        <Switch>
-          <Route exact path="/" component={LoginForm} />
-          <Route exact path="/signup" component={SignupForm} />
-          <Route exact path="/about" component={About} />
-        </Switch>
-      </Router>
->>>>>>> feature/react-building
-    </div>
+      </div> 
   )
 }
 
