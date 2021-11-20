f3f619c06f4be58ecf8a43137074c9200b38ae2a
Brian <bgswilde@gmail.com>
2021-11-19 22:33:08 -0600

    fixed styles in home, header, and footer

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './index.css';
+import { Link } from 'react-router-dom';
 import { Container, Image, Button } from 'react-bootstrap';
+
 
 function Home () {
   return (
     <Container>
         <Image className="main-logo" src="./assets/images/happycheckin-full3.png" fluid></Image>
-        <Button className="homeStartBtn">Get Started!</Button>
+        <Button className="home-start-btn" href= "./login">Get Started!</Button>
     </Container>
   )
 }
