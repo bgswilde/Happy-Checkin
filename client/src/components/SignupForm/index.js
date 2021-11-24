7a590130b75814e3bc329de9e942a0eef453c99c
Brian <bgswilde@gmail.com>
2021-11-24 16:08:27 -0600

    signup page is back, baby

--- client/src/components/SignupForm/index.js
+++ client/src/components/SignupForm/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../../utils/mutations';
 import Auth from '../../utils/auth'
-import { Container, Col, Row, Form, Button, FormGroup } from 'react-bootstrap';
+import { Container, Col, Row, Form, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './index.css';
 
@@ -48,25 +48,31 @@ function SignupForm () {
   }
 
   return(
-    <Container className="log-container">
-      <Col className="form-card">
-        <Form onSubmit={handleFormSubmit}>
-          <FormGroup>
-            <label for="firstName">First Name</label>
-            <input type="firstName" name="firstName" id="firstName" placeholder="Enter your first name" onChange={handleChange} />
-          </FormGroup>
-          <FormGroup>
-            <label for="lastName">Last Name</label>
-            <input type="lastName" name="lastName" id="lastName" placeholder="Enter your last name" onChange={handleChange} />
-          </FormGroup>
-          <FormGroup>
-            <label for="phoneNumber">Phone Number (areacode first)</label>
-            <input type="phoneNumber" name="phoneNumber" id="phoneNumber" placeholder="Phone number(area code first)" onChange={handleChange} />
-          </FormGroup>
-          <FormGroup>
-            <label for="password">Password</label>
-            <input type="password" name="password" id="loginPassword" placeholder="At least 5 characters long" onChange={handleChange} />
-          </FormGroup>
+    <Container>
+      <Row className="justify-content-center">
+        <Col sm="8" xl="6">
+          <h1 className="title">Sign Up</h1>
+        </Col>
+      </Row>
+      <Col md="8" className="form-card log-container">
+        <p className="subtitle">You'll soon kiss checkin frustration goodbye...</p>
+        <Form className="user-details" onSubmit={handleFormSubmit}>
+          <Form.Group className="mb-3">
+            <Form.Label className="mb-0" htmlFor="firstName">First Name</Form.Label>
+            <Form.Control type="text" name="firstName" id="firstName" placeholder="Enter your first name" onBlur={handleChange} />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label className="mb-0" htmlFor="lastName">Last Name</Form.Label>
+            <Form.Control type="text" name="lastName" id="lastName" placeholder="Enter your last name" onBlur={handleChange} />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label className="mb-0" htmlFor="phoneNumber">Phone Number</Form.Label>
+            <Form.Control type="text" name="phoneNumber" id="phoneNumber" placeholder="ex. 9876543210" onBlur={handleChange} />
+          </Form.Group >
+          <Form.Group className="mb-3">
+            <Form.Label className="mb-0" htmlFor="password">Password</Form.Label>
+            <Form.Control type="password" name="password" id="loginPassword" placeholder="At least 5 characters long" onBlur={handleChange} />
+          </Form.Group>
           <Button className="submit-btn" color="info" type="submit">Register</Button>
         </Form>
         {error && <div className="error-msg">sign up failed</div>}
