4f58374ca0f5d025412afe348a600fb9159e6e9b
Will Knowles <will@willknowles.com>
2021-11-24 17:30:25 -0600

    heroku troubleshooting

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
+console.log(`\nmongo: ${process.env.MONGODB_URI}\n`)
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/happy-checkin', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
